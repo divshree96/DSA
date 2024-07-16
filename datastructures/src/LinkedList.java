99eea9650c868191f1df307c095f0a354bef63d9
Divya Shree <shree.d@northeastern.edu>
2024-07-16 17:56:11 -0400

    Linked List initial

---
+++ datastructures/src/LinkedList.java
@@ -0,0 +1,41 @@
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head is " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail is " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length is " + length);
+    }
+}
---
+++ datastructures/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        LinkedList list = new LinkedList(5);
+        list.getHead();
+        list.getTail();
+        list.getLength();
+        list.printList();
+    }
+}
