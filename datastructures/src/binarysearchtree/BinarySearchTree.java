8ede0a93fb9ef1d9121562a25c0d0446821da6c2
Divya Shree <shree.d@northeastern.edu>
2024-07-29 22:52:24 -0400

    BST: Insert

--- datastructures/src/binarysearchtree/BinarySearchTree.java
+++ datastructures/src/binarysearchtree/BinarySearchTree.java
@@ -13,7 +13,30 @@ public class BinarySearchTree {
         }
     }
 
-    public void insert(int value) {
+    public boolean insert(int value) {
         Node node = new Node(value);
+        if(root == null) {
+            root = node;
+            return true;
+        }
+
+        Node temp = root;
+        while(true) {
+            if(node.value == temp.value) return false;
+
+            if(node.value < temp.value) {
+                if(temp.left == null) {
+                    temp.left = node;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if(temp.right == null) {
+                    temp.right = node;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
     }
 }
--- datastructures/src/binarysearchtree/Main.java
+++ datastructures/src/binarysearchtree/Main.java
@@ -3,6 +3,14 @@ package binarysearchtree;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
-        System.out.println("root = " + bst.root);
+        bst.insert(47);
+        bst.insert(21);
+        bst.insert(76);
+        bst.insert(18);
+        bst.insert(52);
+        bst.insert(82);
+        bst.insert(27);
+
+        System.out.println(bst.root.left.right.value);
     }
 }
