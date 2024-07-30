c516ff46c9d136201aec40896d7de6727c76ebe9
Divya Shree <shree.d@northeastern.edu>
2024-07-30 00:22:26 -0400

    HT : Interview Question

--- datastructures/src/hashtable/Main.java
+++ datastructures/src/hashtable/Main.java
@@ -1,5 +1,7 @@
 package hashtable;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         HashTable ht = new HashTable();
@@ -11,13 +13,24 @@ public class Main {
         ht.set("screws", 140);
 
         System.out.println(ht.keys());
-
 //        System.out.println(ht.get("screws")); //140
 //        System.out.println(ht.get("lumber")); //80
 //        System.out.println(ht.get("rubber")); //0
-
-
-
         ht.printTable();
+        System.out.println(itemInCommon(new int[]{1, 3, 5}, new int[]{2, 4, 5}));
+    }
+
+    public static boolean itemInCommon(int[] arr1, int[] arr2) {
+        HashMap<Integer, Boolean> hashMap = new HashMap<>();
+
+        for(int i : arr1) {
+            hashMap.put(i, true);
+        }
+
+        for(int j : arr2) {
+            if(hashMap.get(j) != null) return true;
+        }
+
+        return false;
     }
 }
