15eaba7bf1a4c5fa6db12868b5b52a15fbeb7a18
Divya Shree <shree.d@northeastern.edu>
2024-08-03 15:05:40 -0400

    Graph: remove Edge

--- datastructures/src/graph/Main.java
+++ datastructures/src/graph/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
         graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "C");
+
+        graph.removeEdge("A", "B");
+
         graph.printGraph();
     }
 }
