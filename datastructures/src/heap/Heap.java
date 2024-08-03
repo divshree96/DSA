277f425d02e2ded29b0d21ec2873516649a3984e
Divya Shree <shree.d@northeastern.edu>
2024-08-03 18:26:36 -0400

    Heap : remove

--- datastructures/src/heap/Heap.java
+++ datastructures/src/heap/Heap.java
@@ -43,5 +43,38 @@ public class Heap {
         }
     }
 
+    public Integer remove() {
+        if(heap.size() == 0) return null;
+        if(heap.size() == 1) return heap.remove(0);
+        
+        int maxValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size()-1));
+        sinkDown(0);
+        
+        return maxValue;
+    }
 
+    private void sinkDown(int index) {
+        int maxIndex = index;
+
+        while(true) {
+            int leftIndex = leftChild(maxIndex);
+            int rightIndex = rightChild(maxIndex);
+
+            if(leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+                maxIndex = leftIndex;
+            }
+
+            if(rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+                maxIndex = rightIndex;
+            }
+
+            if(maxIndex != index) {
+                swap(index, maxIndex);
+                index = maxIndex;
+            } else {
+                return ;
+            }
+        }
+    }
 }
